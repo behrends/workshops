2453c9fbf78fbd88278a1f55839631291154e37e
Erik Behrends <behrends@users.noreply.github.com>
2021-11-08 14:50:18 +0100

    WeatherApp: HTML für Wetter-Infos bei neuem Ort

--- exercises/web-prog/WeatherApp/script.js
+++ exercises/web-prog/WeatherApp/script.js
@@ -2,10 +2,22 @@ const search_btn = document.getElementById('search_btn');
 const search_input = document.getElementById('search_input');
 const weather_list = document.getElementById('weather_list');
 
-function addLocation(name) {
+function addLocation(name, temp, condition, image) {
   const new_location = document.createElement('div');
   new_location.classList.add('weather_location');
-  new_location.innerHTML = `<div class="location">${name}</div>`;
+  new_location.innerHTML = `<div class="location">${name}</div>
+    <div class="weather_info">
+      <span>
+        <span class="temperature">${temp} Grad</span>
+        <span class="condition">${condition}</span>
+      </span>
+      <img
+        class="weather_img"
+        src="${image}"
+        alt="${condition}"
+      />
+    </div>
+  `;
   weather_list.appendChild(new_location);
 }
 
@@ -15,7 +27,10 @@ function doSearch() {
   // TODO: Ort nur hinzufügen, wenn Daten gefunden wurden
   // TODO: Fehlermeldung falls keine Daten gefunden wurden
   // TODO: doppelte Orte vermeiden?
-  addLocation(location_name);
+  const temp = 20;
+  const condition = 'sonnig';
+  const image = "https://cdn.glitch.com/c569e324-22c3-491c-ab27-94a3498d6207%2Fsun-line.png?v=1633008214083";
+  addLocation(location_name, temp, condition, image);
 }
 
 search_btn.addEventListener('click', doSearch);
