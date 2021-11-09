136d72b5e598a4ae83beeaf434aa34b0bef46dcd
Erik Behrends <behrends@users.noreply.github.com>
2021-11-09 11:57:44 +0100

    WeatherApp: Liste der Orte dynamisch befüllen

--- exercises/web-prog/WeatherApp/script.js
+++ exercises/web-prog/WeatherApp/script.js
@@ -1,7 +1,9 @@
 const add_btn = document.getElementById('add_btn');
 const search_btn = document.getElementById('search_btn');
 const search_input = document.getElementById('search_input');
-const selected_location = document.getElementById('selected_location');
+const selected_location = document.getElementById(
+  'selected_location'
+);
 const weather_list = document.getElementById('weather_list');
 
 function addLocation(name, temp, condition, image) {
@@ -32,7 +34,8 @@ function doSearch() {
   // TODO: doppelte Orte vermeiden?
   const temp = 20;
   const condition = 'sonnig';
-  const image = "https://cdn.glitch.com/c569e324-22c3-491c-ab27-94a3498d6207%2Fsun-line.png?v=1633008214083";
+  const image =
+    'https://cdn.glitch.com/c569e324-22c3-491c-ab27-94a3498d6207%2Fsun-line.png?v=1633008214083';
   addLocation(location_name, temp, condition, image);
   search_input.value = '';
 }
@@ -43,9 +46,69 @@ function doAddFromList() {
   // TODO: doppelte Orte vermeiden?
   const temp = 13;
   const condition = 'heiter';
-  const image = "https://cdn.glitch.me/c569e324-22c3-491c-ab27-94a3498d6207%2Fsun-cloudy-line.png?v=1634724998045";
+  const image =
+    'https://cdn.glitch.me/c569e324-22c3-491c-ab27-94a3498d6207%2Fsun-cloudy-line.png?v=1634724998045';
   addLocation(location_name, temp, condition, image);
 }
 
 search_btn.addEventListener('click', doSearch);
 add_btn.addEventListener('click', doAddFromList);
+
+let locations = [
+  {
+    name: 'Berlin',
+    latitude: 52.52,
+    longitude: 13.41,
+  },
+  {
+    name: 'Basel',
+    latitude: 47.55,
+    longitude: 7.58,
+  },
+  {
+    name: 'Barcelona',
+    latitude: 41.4,
+    longitude: 2.16,
+  },
+  {
+    name: 'London',
+    latitude: 51.5,
+    longitude: -0.11,
+  },
+  {
+    name: 'Paris',
+    latitude: 48.85,
+    longitude: 2.35,
+  },
+  {
+    name: 'Hamburg',
+    latitude: 53.55,
+    longitude: 9.99,
+  },
+  {
+    name: 'Kopenhagen',
+    latitude: 5.67,
+    longitude: 12.57,
+  },
+  {
+    name: 'Rom',
+    latitude: 41.88,
+    longitude: 12.48,
+  },
+  {
+    name: 'New York',
+    latitude: 40.71,
+    longitude: -74,
+  },
+];
+
+function setupLocationList() {
+  locations.forEach((location, index) => {
+    const option = document.createElement('option');
+    option.innerHTML = location.name;
+    option.setAttribute('value', index);
+    selected_location.appendChild(option);
+  });
+}
+
+setupLocationList();
