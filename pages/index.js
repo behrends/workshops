cdcace5c6ada0ac81858fdbe1964a4d9d7e7b138
Erik Behrends <behrends@users.noreply.github.com>
2022-04-05 18:01:52 +0200

    Minor

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Workshops</title>
+        <title>Workshops…</title>
         <meta name="description" content="Workshop Notizen" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
