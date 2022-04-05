d69703b34f7cf876227985b5959f4b6ee8f08c2b
Erik Behrends <behrends@users.noreply.github.com>
2022-04-05 18:01:33 +0200

    Upgrade tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
