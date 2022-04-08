becb03d7953cd4febbac1775f34f7b9104b17962
Erik Behrends <behrends@users.noreply.github.com>
2022-04-08 19:12:18 +0200

    crossorigin=anonymous

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://EPTBHABXXP-dsn.algolia.net"
-          crossorigin="true"
+          crossorigin="anonymous"
         />
       </Head>
       <body>
