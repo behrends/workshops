7bd53974cf393d6728156bb34a5c464b2107ee7f
Erik Behrends <behrends@users.noreply.github.com>
2022-12-16 15:53:35 +0100

    Component  to edit and run JS Code with Sandpack

---
+++ components/JSEditor.js
@@ -0,0 +1,29 @@
+import {
+  SandpackProvider,
+  SandpackCodeEditor,
+  SandpackConsole,
+  SandpackPreview,
+} from '@codesandbox/sandpack-react';
+import * as Themes from '@codesandbox/sandpack-themes';
+
+export default function JSEditor({ code = '// Code einfügen' }) {
+  return (
+    <SandpackProvider
+      template="vanilla"
+      files={{
+        '/src/index.js': code,
+      }}
+      theme={Themes.githubLight}
+    >
+      <div style={{ border: '1px solid #CCC' }}>
+        <SandpackCodeEditor />
+        <SandpackConsole style={{ maxHeight: '300px' }} />
+        <SandpackPreview
+          style={{ height: '120px' }}
+          showRefreshButton={false}
+          showOpenInCodeSandbox={false}
+        />
+      </div>
+    </SandpackProvider>
+  );
+}
