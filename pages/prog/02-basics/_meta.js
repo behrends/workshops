a5d45ad5f9e0e8a2391bffd0ae8d516d8de0a94c
Erik Behrends <behrends@users.noreply.github.com>
2025-01-07 16:53:58 +0100

    Prop: Umbau zweite Vorlesungseinheit

--- pages/prog/02-basics/_meta.js
+++ pages/prog/02-basics/_meta.js
@@ -1,8 +1,8 @@
 export default {
-  uebung: 'Übung',
-  basics: 'Grundbegriffe',
-  variablen: 'Variablen und Konstanten',
-  io: 'Ein- und Ausgabe',
-  types: 'Datentypen',
-  uebung2: 'Übung',
+  '01-setup': 'Setup Node.js',
+  '02-variablen': 'Variablen und Konstanten',
+  '03-basics': 'Grundbegriffe',
+  '04-io': 'Ein- und Ausgabe',
+  '05-typen': 'Datentypen',
+  '06-uebung': 'Übung',
 };
