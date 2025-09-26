f7439c77117256e8de70fb2673589f5f45ffd35b
Erik Behrends <behrends@users.noreply.github.com>
2025-09-26 17:22:02 +0200

    Mobile: Ordnerstruktur; ;

--- pages/mobile/_meta.js
+++ pages/mobile/_meta.js
@@ -2,7 +2,7 @@ export default {
   index: 'Übersicht',
   '01-intro': '1 - Einleitung',
   '02-android': '2 - Android',
-  '03-android': '3 - Android und KMP',
+  '03-android-kmp': '3 - Android und KMP',
   '04-flutter': '4 - Flutter',
   '05-react-native': '5 - React Native Teil 1',
   '06-react-native': '6 - React Native Teil 2',
