bec92267d931b25b31bee1aa65f3f45de7575372
Erik Behrends <behrends@users.noreply.github.com>
2025-10-01 18:27:34 +0200

    Enhance styling of not found page header

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,7 +3,7 @@ import { NotFoundPage } from 'nextra-theme-docs';
 export default function NotFound() {
   return (
     <NotFoundPage content={null} labels="broken-link">
-      <h1>Seite nicht gefunden</h1>
+      <h1 className="text-3xl font-bold">Seite nicht gefunden</h1>
       <a className="text-blue-500" href="/">
         Zur Startseite &rarr;
       </a>
