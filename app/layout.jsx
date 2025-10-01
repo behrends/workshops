b9770307db4e028a46c04a327d4858f05667d04c
Erik Behrends <behrends@users.noreply.github.com>
2025-10-01 16:53:41 +0200

    Migrate to Nextra 4

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,67 @@
-import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Head } from 'nextra/components'
-import { getPageMap } from 'nextra/page-map'
-import 'nextra-theme-docs/style.css'
- 
+import {
+  Footer,
+  LastUpdated,
+  Layout,
+  Navbar,
+} from 'nextra-theme-docs';
+import { Head } from 'nextra/components';
+import { getPageMap } from 'nextra/page-map';
+import './globals.css';
+
 export const metadata = {
-  // Define your metadata here
-  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
-}
- 
+  title: {
+    default: 'progcontent',
+    template: '%s',
+  },
+  openGraph: {
+    url: 'https://workshops.progcontent.com',
+    siteName: 'Workshops Progcontent',
+    locale: 'de',
+    type: 'website',
+  },
+};
+
 const navbar = (
   <Navbar
-    logo={<b>Nextra</b>}
-    // ... Your additional navbar options
+    logo={<b>Workshops</b>}
+    projectLink="https://github.com/behrends/workshops"
   />
-)
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
- 
+);
+const footer = (
+  <Footer>
+    <span>
+      <abbr
+        title="Dieses Werk ist lizenziert unter einer Creative Commons Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen 4.0 International Lizenz."
+        style={{ cursor: 'help', marginRight: '10px' }}
+      >
+        <a
+          rel="license"
+          href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+          target="_blank"
+        >
+          CC BY-NC-SA 4.0
+        </a>
+      </abbr>
+      <span>
+        {`© ${new Date().getFullYear()} `}
+        <a
+          rel="author"
+          href="https://www.behrends.io"
+          target="_blank"
+        >
+          Erik Behrends
+        </a>
+      </span>
+      <a href="/impressum" className="impressum">
+        Impressum
+      </a>
+    </span>
+  </Footer>
+);
+
 export default async function RootLayout({ children }) {
   return (
     <html
-      // Not required, but good for SEO
       lang="de"
       // Required to be set
       dir="ltr"
@@ -35,13 +77,22 @@ export default async function RootLayout({ children }) {
         <Layout
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/behrends/workshops/tree/main"
+          editLink="Diese Seite bei GitHub →"
+          feedback={{ content: null }}
           footer={footer}
-          // ... Your additional layout options
+          lastUpdated={
+            <LastUpdated locale="de">Zuletzt geändert am</LastUpdated>
+          }
+          sidebar={{ defaultMenuCollapseLevel: 1 }}
+          toc={{
+            title: 'Auf dieser Seite',
+            backToTop: 'Nach oben scrollen',
+          }}
         >
           {children}
         </Layout>
       </body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  content: [
-    './pages/**/*.{md,js,ts,mdx,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-};
