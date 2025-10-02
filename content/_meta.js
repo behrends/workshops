701da43ce8862bceea5556493bcb9587e15320b1
Erik Behrends <behrends@users.noreply.github.com>
2025-10-02 10:22:33 +0200

    Collapse all pages

--- content/_meta.js
+++ content/_meta.js
@@ -1,16 +1,12 @@
 export default {
-  '*': { type: 'page', theme: { timestamp: false } },
+  '*': { type: 'page', theme: { collapsed: true, timestamp: false } },
   impressum: {
     display: 'hidden',
     theme: { sidebar: false, toc: false },
   },
   index: {
     display: 'hidden',
-    theme: {
-      layout: 'full',
-      toc: false,
-      typesetting: 'article',
-    },
+    theme: { layout: 'full', toc: false, typesetting: 'article' },
   },
   prog: 'Programmieren',
   'web-prog': 'Web-Programmierung',
