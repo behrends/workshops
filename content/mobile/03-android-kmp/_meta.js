7037ec2b2f812e7441ee83c22c75731a51f17b93
Erik Behrends <behrends@users.noreply.github.com>
2025-10-08 12:26:17 +0200

    Mobile: noch ein paar Nachträge

--- content/mobile/03-android-kmp/_meta.js
+++ content/mobile/03-android-kmp/_meta.js
@@ -1,7 +1,7 @@
 export default {
-  '01-storage': 'Datenbanken mit Room verwalten',
-  '02-storage2': 'Verwendung von Room',
+  '01-room': 'Datenbanken mit Room verwalten',
+  '02-room2': 'Verwendung von Room',
   '03-navigation': 'Navigation',
-  '04-links': 'Lernressourcen',
+  '04-links': 'Ausblick und Lernressourcen',
   '05-kmp': 'Kotlin Multiplatform (KMP)',
 };
