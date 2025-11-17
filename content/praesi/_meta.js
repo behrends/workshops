4f7459b60406d004d7a19bcfbc18b80b1d67a5ef
Erik Behrends <behrends@users.noreply.github.com>
2025-11-17 19:18:35 +0100

    Präsi: separators

--- content/praesi/_meta.js
+++ content/praesi/_meta.js
@@ -1,13 +1,29 @@
 export default {
   index: 'Übersicht',
+  section1: {
+    type: 'separator',
+    title: '',
+  },
   '00-einstieg': 'Einstieg (4 Std.)',
   '01-demos': 'Demos und Beispiele (4 Std.)',
   demos: 'Live-Beispiele',
+  section2: {
+    type: 'separator',
+    title: '',
+  },
   '02-themen': 'Themen und Teams (4 Std.)',
   themen: 'Themenlisten',
   '03-infos': 'Infos und Teamarbeit (4 Std.)',
+  section3: {
+    type: 'separator',
+    title: '',
+  },
   '04-beispiel': 'Beispiel und Teamarbeit (4 Std.)',
   '05-team-arbeit': 'Teamarbeit (4 Std.)',
+  section4: {
+    type: 'separator',
+    title: '',
+  },
   '06-praesi': 'Präsentationen (6 Std.)',
   rest: 'Sonstiges',
 };
