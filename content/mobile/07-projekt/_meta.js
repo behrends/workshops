cbe69ef41b324920e59211521a461536248eef51
Erik Behrends <behrends@users.noreply.github.com>
2025-12-03 17:45:17 +0100

    Mobile: Testing

--- content/mobile/07-projekt/_meta.js
+++ content/mobile/07-projekt/_meta.js
@@ -2,6 +2,7 @@ export default {
   '01-ki-tools': 'KI-Tools',
   '02-teamarbeit': 'Teamarbeit',
   '03-backend-loesungen': 'Backend-Lösungen',
-  '04-test-bibliotheken': 'Test-Bibliotheken',
-  '05-apk-erstellung': 'APK-Erstellung',
+  '04-test-react-native': 'Tests mit React Native',
+  '05-test-android': 'Testen nativer Android Apps',
+  '06-apk-erstellung': 'APK-Erstellung',
 };
