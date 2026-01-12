239d54757e497547eec8c06791d04b4814595b8f
Erik Behrends <behrends@users.noreply.github.com>
2026-01-12 16:30:44 +0100

    Prog: Umstrukturierungen zu Beginn

--- content/prog/01-intro/_meta.js
+++ content/prog/01-intro/_meta.js
@@ -2,8 +2,7 @@ export default {
   '01-vorstellung': 'Vorstellung',
   '02-motivation': 'Programmieren im KI-Zeitalter',
   '03-organisatorisches': 'Organisatorisches',
-  '04-ablauf': 'Ablauf',
-  '05-intro': 'Einführung',
-  '06-javascript': 'JavaScript',
-  '07-hello_world': 'Hello, World!',
+  '04-intro': 'Einführung',
+  '05-javascript': 'JavaScript',
+  '06-hello_world': 'Hello, World!',
 };
