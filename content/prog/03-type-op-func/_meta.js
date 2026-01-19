786f723658607beac76197a51b478271df837bd9
Erik Behrends <behrends@users.noreply.github.com>
2026-01-19 12:22:03 +0100

    Prog: Add mini-project: Trinkgeldrechner and restructure functions section

--- content/prog/03-type-op-func/_meta.js
+++ content/prog/03-type-op-func/_meta.js
@@ -1,6 +1,7 @@
 export default {
   '01-typen': 'Datentypen',
   '02-operators': 'Operatoren',
-  '03-functions': 'Funktionen (Teil 1)',
-  '04-uebungen': 'Übungen',
+  '03-trinkgeld': 'Mini-Projekt: Trinkgeldrechner',
+  '04-functions': 'Funktionen (Teil 1)',
+  '05-uebungen': 'Übungen',
 };
