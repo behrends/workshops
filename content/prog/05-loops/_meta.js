638b179230252af9293fa4873c272b3d63745aa0
Erik Behrends <behrends@users.noreply.github.com>
2026-01-21 20:00:21 +0100

    Prog: Einzeilaufgaben nach while

--- content/prog/05-loops/_meta.js
+++ content/prog/05-loops/_meta.js
@@ -2,4 +2,5 @@ export default {
   '01-while': 'while-Schleifen',
   '02-zahlenraten': 'Mini-Projekt: Zahlenraten',
   '03-zinsrechner': 'Mini-Projekt: Zinsrechner',
+  '04-einzelaufgaben': 'Einzelaufgaben',
 };
