ea3f5589f61e6f869eb7902604568b12f1348506
Erik Behrends <behrends@users.noreply.github.com>
2026-01-21 19:00:33 +0100

    Prog: while aufgeräumt

--- content/prog/05-loops/_meta.js
+++ content/prog/05-loops/_meta.js
@@ -1,7 +1,5 @@
 export default {
-  '01-calculator': 'Übung (Taschenrechner)',
-  '02-while': 'Schleifen (while)',
-  '03-calculator': 'Übung',
-  '04-zahlenraten': 'Mini-Projekt: Zahlenraten',
-  '05-zinsrechner': 'Mini-Projekt: Zinsrechner',
+  '01-while': 'while-Schleifen',
+  '02-zahlenraten': 'Mini-Projekt: Zahlenraten',
+  '03-zinsrechner': 'Mini-Projekt: Zinsrechner',
 };
