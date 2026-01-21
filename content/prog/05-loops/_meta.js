7c56d4b25535b632a96d6af2737a362fdfe4b650
Erik Behrends <behrends@users.noreply.github.com>
2026-01-21 18:24:06 +0100

    Prog: Mini-Projekte Zahlenraten und Zinsrechner hinzugefügt
    Zwei neue Übungen für Abschnitt 05-loops:
    - Zahlenraten: Interaktives Spiel mit while, if/else, Math.random()
    - Zinsrechner: Praktische Finanzberechnungen mit Zinseszins

--- content/prog/05-loops/_meta.js
+++ content/prog/05-loops/_meta.js
@@ -2,4 +2,6 @@ export default {
   '01-calculator': 'Übung (Taschenrechner)',
   '02-while': 'Schleifen (while)',
   '03-calculator': 'Übung',
+  '04-zahlenraten': 'Mini-Projekt: Zahlenraten',
+  '05-zinsrechner': 'Mini-Projekt: Zinsrechner',
 };
