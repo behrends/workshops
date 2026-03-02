a500e9b7e3c8fcf6adf3b19601e8aa54c8162efc
Erik Behrends <behrends@users.noreply.github.com>
2026-03-02 19:30:19 +0100

    Prog: WIP unit testing

--- content/prog/09-quality/_meta.js
+++ content/prog/09-quality/_meta.js
@@ -2,7 +2,6 @@ export default {
   '01-intro': 'Einführung und Testpyramide',
   '02-typescript': 'TypeScript',
   '03-eslint': 'Linting mit ESLint',
-  '04-unit-testing-vitest': 'Unit-Testing mit Vitest',
-  '05-unit-testing': 'Unit-Testing mit node:test',
+  '05-unit-testing': 'Unit-Testing',
   '05-ausblick': 'Ausblick',
 };
