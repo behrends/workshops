4adf25e00cad73329b71ca41626273a6411047d0
Erik Behrends <behrends@users.noreply.github.com>
2026-03-02 13:03:42 +0100

    Rename intro-testpyramide file

--- content/prog/09-quality/_meta.js
+++ content/prog/09-quality/_meta.js
@@ -1,5 +1,5 @@
 export default {
-  '01-intro-testpyramide': 'Einführung und Testpyramide',
+  '01-intro': 'Einführung und Testpyramide',
   '02-typescript': 'TypeScript',
   '03-eslint': 'Linting mit ESLint',
   '04-unit-testing-vitest': 'Unit-Testing mit Vitest',
