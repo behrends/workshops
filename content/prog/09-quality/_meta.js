562bcb6bd3f787043a118f1fe365198feb8c1ccb
Erik Behrends <behrends@users.noreply.github.com>
2026-03-02 16:59:21 +0100

    Prog: remove temporary section

--- content/prog/09-quality/_meta.js
+++ content/prog/09-quality/_meta.js
@@ -3,5 +3,6 @@ export default {
   '02-typescript': 'TypeScript',
   '03-eslint': 'Linting mit ESLint',
   '04-unit-testing-vitest': 'Unit-Testing mit Vitest',
+  '05-unit-testing': 'Unit-Testing mit node:test',
   '05-ausblick': 'Ausblick',
 };
--- content/prog/09b-quality/_meta.js
+++
@@ -1,8 +0,0 @@
-export default {
-  '01-einfuehrung': 'Einführung und Testpyramide',
-  '02-prettier': 'Prettier (Formatierung)',
-  '03-typescript': 'TypeScript (Statische Typen)',
-  '04-eslint': 'ESLint (Linting)',
-  '05-unit-testing': 'Unit Testing',
-  '06-ausblick': 'Ausblick',
-};
